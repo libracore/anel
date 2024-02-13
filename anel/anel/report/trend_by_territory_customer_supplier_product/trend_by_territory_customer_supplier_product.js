e8896bbd9ff038646c51e92edc38ca6fc138fd35
lasalesi <larsericmueller@gmail.com>
2024-02-13 22:21:17 +0100

    new report trend by territory-customer-supplier-product

---
+++ anel/anel/report/trend_by_territory_customer_supplier_product/trend_by_territory_customer_supplier_product.js
@@ -0,0 +1,44 @@
+// Copyright (c) 2016, libracore and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Trend by Territory-Customer-Supplier-Product"] = {
+    "filters": [
+        {
+            "fieldname":"p1_from_date",
+            "label": __("From Date P1"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.add_months(frappe.datetime.get_today(), -2),
+            "reqd": 1
+        },
+                {
+            "fieldname":"p1_to_date",
+            "label": __("To Date P1"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.get_today(),
+            "reqd": 1
+        },
+        {
+            "fieldname":"p2_from_date",
+            "label": __("From Date P2"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.add_months(frappe.datetime.get_today(), -6),
+            "reqd": 1
+        },
+        {
+            "fieldname":"p2_to_date",
+            "label": __("To Date P2"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.get_today(),
+            "reqd": 1
+        }
+    ],
+    "onload": (report) => {
+        var inner_toolbars = document.getElementsByClassName("form-inner-toolbar");
+        if (inner_toolbars) {
+            for (var i = 0; i < inner_toolbars.length; i++) {
+                inner_toolbars[i].innerHTML = "<div class=\"form-message text-muted small blue\">Absatz auf Stufe Ausgangsrechnung</div>";
+            }
+        }
+    }
+};
